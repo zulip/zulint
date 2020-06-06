zulip/zulint

commit 35120a2c690e0fa90d6ce1b610a5a3bd62814e63

list_files: Use git ls-files -z; exclude all symlinks.
The previous code only excluded broken symlinks.

Signed-off-by: Anders Kaseorg <anders@zulip.com>

zulint/lister.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import stat
 import sys
 import subprocess
 import re
@@ -99,9 +100,12 @@ def list_files(
     if modified_only:
         cmdline.append('-m')
 
-    files_gen = (x.strip() for x in subprocess.check_output(cmdline, universal_newlines=True).split('\n'))
-    # throw away empty lines and non-files (like symlinks)
-    files = list(filter(os.path.isfile, files_gen))
+    files = subprocess.check_output(
+        ["git", "ls-files", "-z"], encoding="utf-8",
+    ).split("\0")
+    assert files.pop() == ""
+    # throw away non-files (like symlinks)
+    files = [f for f in files if stat.S_ISREG(os.lstat(f).st_mode)]
 
     result_dict = defaultdict(list)  # type: Dict[str, List[str]]
     result_list = []  # type: List[str]

commit 2c8789be52dd3e4873c0f7598cb6d2c56c054656

lister: Filter using specified targets.
35120a2c690e0fa90d6ce1b610a5a3bd62814e63 introduced a regression
which lists all files regardless of the targets specified. We fix it in this
commit by passing the targets too.

zulint/lister.py
@@ -96,12 +96,12 @@ def list_files(
                                                '--show-toplevel']).strip().decode('utf-8')
     exclude_abspaths = [os.path.abspath(os.path.join(repository_root, fpath)) for fpath in exclude]
 
-    cmdline = ['git', 'ls-files'] + targets
+    cmdline = ["git", "ls-files", "-z"] + targets
     if modified_only:
         cmdline.append('-m')
 
     files = subprocess.check_output(
-        ["git", "ls-files", "-z"], encoding="utf-8",
+        cmdline, encoding="utf-8",
     ).split("\0")
     assert files.pop() == ""
     # throw away non-files (like symlinks)